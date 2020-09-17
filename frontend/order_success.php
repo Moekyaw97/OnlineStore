914a3c3fa61d10a9fefebf45ea5ea8465f888b7f
Moekyaw97 <71075675+Moekyaw97@users.noreply.github.com>
2020-09-17 17:58:46 +0630

    Update order_success.php

--- frontend/order_success.php
+++ frontend/order_success.php
@@ -1,14 +1,16 @@
 <?php 
 include 'include/head.php';
  ?>
-
- <div class="container my-5 text-center">
+<div style="background-color: #ffffff;">
+ <div class="container  text-center">
  	<h3 class="text-success pt-5">Your Order is  Success</h3>
  	<p>Your Order is Submited Successful.We will deliver to you within 3 days.</p>
- 	<a href="product.php" class="btn btn-success mb-5">Continue Shopping</a>
+ 	<a href="product.php" class="btn btn-success">Continue Shopping</a>
  </div>
-
-
+ <div class="text-center">
+ <img src="image/success-tick-dribbble.gif" class="img-fluid">
+</div>
+</div>
   
 
   <?php 
